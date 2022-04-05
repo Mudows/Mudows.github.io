a1a1ac14af729eb59c25ced6a9b90a6542bf6c55
Diego M. Cezar <dmcezar@gmail.com>
2022-04-04 23:29:09 -0300

    Jogo RPS consertado.

--- simple-games/js/rps-game.js
+++ simple-games/js/rps-game.js
@@ -1,16 +1,16 @@
-const cpuChoiceDisplay = document.getElementById("cpu-choice");
-const plrChoiceDisplay = document.getElementById("plr-choice");
-const gameResultDisplay = document.getElementById("result");
-const nChoices = document.querySelectorAll('.rps-buttons');
+const cpuChoiceDisplay = document.getElementById('cpu-choice');
+const plrChoiceDisplay = document.getElementById('plr-choice');
+const gameResultDisplay = document.getElementById('result');
+const nChoices = document.querySelectorAll('.rps-p-choice');
 let plrChoice;
 let cpuChoice;
 let cpuChoiceImg;
 let gameResult;
 
 nChoices.forEach((nChoice) =>
-  nChoice.addEventListener("click", (e) => {
-    plrChoice = e.target.innerHTML;
-    plrChoiceDisplay.innerHTML = plrChoice;
+  nChoice.addEventListener('click', (e) => {
+    plrChoice = e.target.id;
+    plrChoiceDisplay.src = e.target.src;
     generateCpuChoice();
     getResult();
   })
@@ -20,45 +20,42 @@ function generateCpuChoice() {
   const rndNumber = Math.floor(Math.random() * 3) + 1;
 
   if (rndNumber === 1) {
-    cpuChoice = "Rock";
-    cpuChoiceImg = document.createElement('img');
-    cpuChoiceImg.src = img/rock.png;
+    cpuChoice = 'Rock';
+    cpuChoiceImg = 'img/rps/rock.png';
   }
 
   if (rndNumber === 2) {
-    cpuChoice = "Paper";
-    cpuChoiceImg = document.createElement('img');
-    cpuChoiceImg.src = "img/paper.png";
+    cpuChoice = 'Paper';
+    cpuChoiceImg = 'img/rps/paper.png';
   }
 
   if (rndNumber === 3) {
-    cpuChoice = "Scissors";
-    cpuChoiceImg = document.createElement('img');
-    cpuChoiceImg.src = 'img/scissor.png';
+    cpuChoice = 'Scissors';
+    cpuChoiceImg = 'img/rps/scissor.png';
   }
 
-  cpuChoiceDisplay.innerHTML = cpuChoiceImg;
+  cpuChoiceDisplay.src = cpuChoiceImg;
 }
 
 function getResult() {
   if (cpuChoice === plrChoice) {
-    gameResult = "Draw!";
+    gameResult = 'Draw!';
   }
 
   if (
-    (cpuChoice === "Rock" && plrChoice === "Scissors") ||
-    (cpuChoice === "Paper" && plrChoice === "Rock") ||
-    (cpuChoice === "Scissors" && plrChoice === "Paper")
+    (cpuChoice === 'Rock' && plrChoice === 'scissors') ||
+    (cpuChoice === 'Paper' && plrChoice === 'rock') ||
+    (cpuChoice === 'Scissors' && plrChoice === 'paper')
   ) {
-    gameResult = "You lose!";
+    gameResult = 'You lost!';
   }
 
   if (
-    (cpuChoice === "Scissors" && plrChoice === "Rock") ||
-    (cpuChoice === "Rock" && plrChoice === "Paper") ||
-    (cpuChoice === "Paper" && plrChoice === "Scissors")
+    (cpuChoice === 'Scissors' && plrChoice === 'rock') ||
+    (cpuChoice === 'Rock' && plrChoice === 'paper') ||
+    (cpuChoice === 'Paper' && plrChoice === 'scissors')
   ) {
-    gameResult = "You win!!!";
+    gameResult = 'You won!!!';
   }
 
   gameResultDisplay.innerHTML = gameResult;
