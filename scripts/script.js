fb72b11e9f4e7685382ed3f8be37590a65279192
Diego Moraes Cezar <dmcezar@gmail.com>
2022-06-14 22:33:10 -0300

    Simples refatoração do código jQuery.

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,17 @@ const validateEmailInput = () => {
   const clientEmail = $('#client-email').val();
   const emailValidation = /\S+@\S+\.\S+/;
   if (emailValidation.test(clientEmail)) {
-    $('#submit-button').removeClass('btn-secondary');
-    $('#submit-button').addClass('btn-primary');
-    return ($('#submit-button').prop('disabled', false));
+    $('#submit-button')
+      .removeClass('btn-secondary')
+      .addClass('btn-primary')
+      .prop('disabled', false);
+    return;
   }
-  $('#submit-button').removeClass('btn-primary');
-  $('#submit-button').addClass('btn-secondary');
-  return ($('#submit-button').prop('disabled', true));
+  $('#submit-button')
+    .removeClass('btn-primary')
+    .addClass('btn-secondary')
+    .prop('disabled', true);
+  return;
 };
 
 $('#client-email').on('input', validateEmailInput);
